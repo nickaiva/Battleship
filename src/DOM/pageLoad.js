abba7e0e67a9257f04cc8c5ea5c2c7206089aec6
Nick Aiva <nickaiva@yahoo.gr>
2021-06-13 10:47:49 +0300

    Initial version

---
+++ src/DOM/pageLoad.js
@@ -0,0 +1,21 @@
+"use strict"
+import Player from '../Player';
+import { createButtons, createDOMBoard, winner } from './dom';
+
+export default function load() {
+  const container = document.querySelector('.container');
+  const { row: boardDOM, gPlayer, gEnemy } = createDOMBoard();
+
+  const { row: buttonsDOM, startBtn } = createButtons();
+  const { winnerDOM, winnerText } = winner();
+  container.appendChild(boardDOM);
+  container.appendChild(buttonsDOM);
+  container.appendChild(winnerDOM);
+
+  const player = Player('Admiral', false);
+  const computer = Player('Computer', true);
+  player.grid = gPlayer;
+  computer.grid = gEnemy;
+  const players = [player, computer];
+  return { startBtn, players, winnerDOM, winnerText };
+}
---
+++ src/Player.js
@@ -0,0 +1,39 @@
+"use strict";
+import GameBoard from './Gameboard';
+import { arrInclude } from './utilityFunctions';
+
+const Player = function(playerName, computer)  {
+  const name = playerName;
+  const shots = [];
+
+  const gameBoard = GameBoard();
+  const shot = (xCoord, yCoord) => {
+    let x = xCoord;
+    let y = yCoord;
+    let arr = null;
+    if (computer) {
+      do {
+        x = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+        y = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+        arr = [x, y];
+      } while (arrInclude(shots, arr) === true);
+    } else {
+      if (xCoord == null) return null;
+      arr = [x, y];
+      if (arrInclude(shots, arr)) {
+        return null;
+      }
+    }
+    shots.push([x, y]);
+    return { x: arr[0], y: arr[1] };
+  };
+
+  const reset = () => {
+    shots.length = 0;
+    gameBoard.reset();
+  };
+
+  return { name, shot, shots, gameBoard, reset };
+};
+
+export default Player;
